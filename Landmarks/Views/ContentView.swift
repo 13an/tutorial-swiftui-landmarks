434cfde4d86973581280ca4c6e446e0a49cdff3f
13an <banjoyatakumi@gmail.com>
2023-07-05 17:50:15 +0900

    done this section: Building Lists and Navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 13an on 2023/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
