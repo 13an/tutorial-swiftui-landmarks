558fa63355b79d83037a61929cd4a0ececc795cd
13an <banjoyatakumi@gmail.com>
2023-07-05 14:31:28 +0900

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 13an on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
